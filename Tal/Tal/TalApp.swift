e776015218b7b85dbffbf63c72568729fd8ddd0e
pablojosecodes <pablojrhansen@gmail.com>
2024-02-13 17:02:41 -0800

    Init

---
+++ Tal/Tal/TalApp.swift
@@ -0,0 +1,20 @@
+//
+//  TalApp.swift
+//  Tal
+//
+//  Created by Pablo Hansen on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct TalApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
